9d5cfeb983daf365dc74d13b0ad12d7675974f74
sid12399 <sid.pittie@gmail.com>
2025-06-10 16:11:47 -0700

    improved formatting in the match commands section

--- src/parser.rs
+++ src/parser.rs
@@ -10,68 +10,71 @@ impl Parser {
     }
 
     pub fn process_command(&mut self, raw_cmd: &str) -> Result<&str, &str> {
-        // Get first command
+        // Split the command into a list of tokens
         let cmd_words: Vec<&str> = raw_cmd.trim().split(' ').collect();
     
         if cmd_words.len() == 0 {
             println!("Error: Empty Command");
             return Err("Empty Command");
         }
-    
+        
+        // Match the first word with available commands
         match cmd_words[0] {
-            "GET"  => {
-                         if cmd_words.len() != 2 {
-                             println!("Invalid number of arguments. Required: 1, Found: {}", cmd_words.len() - 1);
-                             return Err("Incorrect number of arguments");
-                         }
-                        
-                         match self.get(cmd_words[1]) {
-                             Some(val) => println!("Value: {}", val),
-                             None            => println!("Key not found")
-                         }
-                      },
+            "GET"       =>  {
+                                if cmd_words.len() != 2 {
+                                    println!("Invalid number of arguments. Required: 1, Found: {}", cmd_words.len() - 1);
+                                    return Err("Incorrect number of arguments");
+                                }
+                            
+                                match self.get(cmd_words[1]) {
+                                    Some(val) => println!("Value: {}", val),
+                                    None            => println!("Key not found")
+                                }
+                            },
             
-            "SET"  => {
-                        if cmd_words.len() != 3 {
-                            println!("Invalid number of arguments. Required: 2, Found: {}", cmd_words.len() - 1);
-                            return Err("Incorrect number of arguments");
-                        }
+            "SET"       =>  {
+                                if cmd_words.len() != 3 {
+                                    println!("Invalid number of arguments. Required: 2, Found: {}", cmd_words.len() - 1);
+                                    return Err("Incorrect number of arguments");
+                                }
 
-                        self.set(cmd_words[1], cmd_words[2]);
-                      }
-
-            "EXISTS"  => {
-                            if cmd_words.len() != 2 {
-                                println!("Invalid number of arguments. Required: 1, Found: {}", cmd_words.len() - 1);
-                                return Err("Incorrect number of arguments");
+                                self.set(cmd_words[1], cmd_words[2]);
                             }
 
-                            if self.exists(cmd_words[1]) {
-                                println!("Key {} exists.", cmd_words[1]);
-                            }
-                            else {
-                                println!("Key does not exist.");
-                            }
-                         } 
+            "EXISTS"    =>  {
+                                if cmd_words.len() != 2 {
+                                    println!("Invalid number of arguments. Required: 1, Found: {}", cmd_words.len() - 1);
+                                    return Err("Incorrect number of arguments");
+                                }
+
+                                if self.exists(cmd_words[1]) {
+                                    println!("Key {} exists.", cmd_words[1]);
+                                }
+                                else {
+                                    println!("Key does not exist.");
+                                }
+                            } 
     
-            "DEL"     => {
-                            if cmd_words.len() != 2 {
-                                println!("Invalid number of arguments. Required: 1, Found: {}", cmd_words.len() - 1);
-                                return Err("Incorrect number of arguments");
-                            }
+            "DEL"       =>  {
+                                if cmd_words.len() != 2 {
+                                    println!("Invalid number of arguments. Required: 1, Found: {}", cmd_words.len() - 1);
+                                    return Err("Incorrect number of arguments");
+                                }
 
-                            match self.del(cmd_words[1]) {
-                                Ok(val) => println!("Deleted key with value: {}", val.as_str()),
-                                Err(err)  => return Err(err)
+                                match self.del(cmd_words[1]) {
+                                    Ok(val) => println!("Deleted key with value: {}", val.as_str()),
+                                    Err(err)  => return Err(err)
+                                }
+                            } 
+    
+            "PING"      =>  {
+                                println!("PONG")
                             }
-                         } 
     
-            "PING" => println!("PONG"),
-    
-            _      => {
-                        println!("Error: {} is an invalid comand", cmd_words[0]);
-                        return Err("Invalid Command");
-                      }
+            _           =>  {
+                                println!("Error: {} is an invalid comand", cmd_words[0]);
+                                return Err("Invalid Command");
+                            }
         }
     
         return Ok("Success");
